b4aa1e1fed3462a9fe2038d3ef39e29309db57db
TanyaNarom <tanyanarom@gmail.com>
2025-09-05 18:16:59 +0200

    Clean repo: Add only required files

--- insert_data.sh
+++ insert_data.sh
@@ -7,31 +7,36 @@ else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
-# Очистка таблиц
-$PSQL "TRUNCATE games, teams"
-
-# Чтение файла games.csv
-cat games.csv | while IFS="," read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+# Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE games, teams")
+cat games.csv | while IFS="," read  YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  if [[ $YEAR != "year" ]]
-  then
-    # Проверяем победителя
-    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    if [[ -z $WINNER_ID ]]
-    then
-      $PSQL "INSERT INTO teams(name) VALUES('$WINNER')"
-      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    fi
+ if [[ $YEAR != 'year' ]]
+ then
+    TEAM_ID_AS_WINNER=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    TEAM_ID_AS_OPPONENT=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+   
+      if [[ -z $TEAM_ID_AS_WINNER ]]
+      then
+        INSERT_TEAMS=$($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
+        TEAM_ID_AS_WINNER=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+        echo INSERTED WINNER, $TEAM_ID_AS_WINNER
+      fi
+      if [[ -z $TEAM_ID_AS_OPPONENT ]]
+      then
+        INSERT_TEAMS=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
+        TEAM_ID_AS_OPPONENT=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+        echo INSERTED OPPONENT, $TEAM_ID_AS_OPPONENT
+      fi
 
-    # Проверяем соперника
-    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    if [[ -z $OPPONENT_ID ]]
-    then
-      $PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')"
-      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    fi
-
-    # Вставляем игру
-    $PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)"
-  fi
+      GAMES_ID=$($PSQL "SELECT game_id FROM games WHERE winner_id=$TEAM_ID_AS_WINNER AND opponent_id=$TEAM_ID_AS_OPPONENT")
+      echo $GAMES_ID
+      if [[ -z $GAMES_ID ]]
+      then
+        INSERT_GAME=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES ($YEAR,'$ROUND',$TEAM_ID_AS_WINNER,$TEAM_ID_AS_OPPONENT,$WINNER_GOALS,$OPPONENT_GOALS)")
+        #GAMES_ID=$($PSQL "SELECT game_id FROM games WHERE winner_id=$TEAM_ID_AS_WINNER AND opponent_id=$TEAM_ID_AS_OPPONENT")
+        echo INSERTED GAME
+      fi
+    
+ fi 
 done
